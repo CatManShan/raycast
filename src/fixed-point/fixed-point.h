9f14d23d50386850eed1f06593a99787e66d96a4
catmanshan <1ionu.47@gmail.com>
2021-03-24 02:13:55 -0600

    Implemented fixed point multiplication

--- src/fixed-point/fixed-point.h
+++ src/fixed-point/fixed-point.h
@@ -11,6 +11,9 @@ struct UFixedPoint64 {
 	uint64_t as_uint;
 };
 
+struct FixedPoint64 fixed_point_construct(int32_t integer, uint32_t fraction);
+struct UFixedPoint64 ufixed_point_construct(uint32_t integer, uint32_t fraction);
+
 double fixed_point64_to_double(struct FixedPoint64 value);
 struct FixedPoint64 fixed_point64_from_double(double value);
 double ufixed_point64_to_double(struct UFixedPoint64 value);
@@ -20,7 +23,6 @@ struct FixedPoint64 fixed_point64_add(struct FixedPoint64 a, struct FixedPoint64
 struct FixedPoint64 fixed_point64_subtract(struct FixedPoint64 a, struct FixedPoint64 b);
 struct FixedPoint64 fixed_point64_multiply(struct FixedPoint64 a, struct FixedPoint64 b);
 struct FixedPoint64 fixed_point64_divide(struct FixedPoint64 a, struct FixedPoint64 b);
-
 struct UFixedPoint64 ufixed_point64_add(struct UFixedPoint64 a, struct UFixedPoint64 b);
 struct UFixedPoint64 ufixed_point64_subtract(struct UFixedPoint64 a, struct UFixedPoint64 b);
 struct UFixedPoint64 ufixed_point64_multiply(struct UFixedPoint64 a, struct UFixedPoint64 b);
