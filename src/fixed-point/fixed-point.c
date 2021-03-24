3bd434947540db57d45d6c056fea5b5a1e96576e
catmanshan <1ionu.47@gmail.com>
2021-03-23 23:29:19 -0600

    Implemented fixed-point addition and subtraction

--- src/fixed-point/fixed-point.c
+++ src/fixed-point/fixed-point.c
@@ -34,14 +34,24 @@ struct UnsignedFixedPoint64 unsigned_fixed_point64_from_double(double value)
 	return REINTERPRET(fp_as_uint, struct UnsignedFixedPoint64);
 }
 
-// struct FixedPoint64 fixed_point64_add(struct FixedPoint64 a, struct FixedPoint64 b)
-// {
-// }
-// 
-// struct FixedPoint64 fixed_point64_subtract(struct FixedPoint64 a, struct FixedPoint64 b)
-// {
-// }
-// 
+struct FixedPoint64 fixed_point64_add(struct FixedPoint64 a, struct FixedPoint64 b)
+{
+	int64_t a_as_int = REINTERPRET(a, int64_t);
+	int64_t b_as_int = REINTERPRET(b, int64_t);
+	int64_t sum = a_as_int + b_as_int;
+
+	return REINTERPRET(sum, struct FixedPoint64);
+}
+
+struct FixedPoint64 fixed_point64_subtract(struct FixedPoint64 a, struct FixedPoint64 b)
+{
+	int64_t a_as_int = REINTERPRET(a, int64_t);
+	int64_t b_as_int = REINTERPRET(b, int64_t);
+	int64_t difference = a_as_int - b_as_int;
+
+	return REINTERPRET(difference, struct FixedPoint64);
+}
+
 // struct FixedPoint64 fixed_point64_multiply(struct FixedPoint64 a, struct FixedPoint64 b)
 // {
 // }
@@ -49,4 +59,31 @@ struct UnsignedFixedPoint64 unsigned_fixed_point64_from_double(double value)
 // struct FixedPoint64 fixed_point64_divide(struct FixedPoint64 a, struct FixedPoint64 b)
 // {
 // }
+
+struct UnsignedFixedPoint64 unsigned_fixed_point64_add(struct UnsignedFixedPoint64 a, struct UnsignedFixedPoint64 b)
+{
+	uint64_t a_as_int = REINTERPRET(a, uint64_t);
+	uint64_t b_as_int = REINTERPRET(b, uint64_t);
+	uint64_t sum = a_as_int + b_as_int;
+
+	return REINTERPRET(sum, struct UnsignedFixedPoint64);
+}
+
+struct UnsignedFixedPoint64 unsigned_fixed_point64_subtract(struct UnsignedFixedPoint64 a, struct UnsignedFixedPoint64 b)
+{
+	uint64_t a_as_int = REINTERPRET(a, uint64_t);
+	uint64_t b_as_int = REINTERPRET(b, uint64_t);
+	uint64_t difference = a_as_int - b_as_int;
+
+	return REINTERPRET(difference, struct UnsignedFixedPoint64);
+}
+
+// struct UnsignedFixedPoint64 unsigned_fixed_point64_multiply(struct UnsignedFixedPoint64 a, struct UnsignedFixedPoint64 b)
+// {
+// }
+// 
+// struct UnsignedFixedPoint64 unsigned_fixed_point64_divide(struct UnsignedFixedPoint64 a, struct UnsignedFixedPoint64 b)
+// {
+// }
+
 
--- src/fixed-point/fixed-point.h
+++ src/fixed-point/fixed-point.h
@@ -23,5 +23,10 @@ struct FixedPoint64 fixed_point64_subtract(struct FixedPoint64 a, struct FixedPo
 struct FixedPoint64 fixed_point64_multiply(struct FixedPoint64 a, struct FixedPoint64 b);
 struct FixedPoint64 fixed_point64_divide(struct FixedPoint64 a, struct FixedPoint64 b);
 
+struct UnsignedFixedPoint64 unsigned_fixed_point64_add(struct UnsignedFixedPoint64 a, struct UnsignedFixedPoint64 b);
+struct UnsignedFixedPoint64 unsigned_fixed_point64_subtract(struct UnsignedFixedPoint64 a, struct UnsignedFixedPoint64 b);
+struct UnsignedFixedPoint64 unsigned_fixed_point64_multiply(struct UnsignedFixedPoint64 a, struct UnsignedFixedPoint64 b);
+struct UnsignedFixedPoint64 unsigned_fixed_point64_divide(struct UnsignedFixedPoint64 a, struct UnsignedFixedPoint64 b);
+
 #endif // fixed_point_h
 
